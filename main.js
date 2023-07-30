b8077de5a981d9ad496f8ab7c2295cb40159d42f
bethiz <108011730+bethiz@users.noreply.github.com>
2023-07-30 09:43:50 -0700

    Add files via upload
    upgraded version of list to do

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//for login html & indexhtml
+{//for login html & indexhtml
 
 const myform=document.querySelector('#my-form');
 const firstnameinput=document.querySelector('#firstname');
@@ -26,5 +26,35 @@ function onSubmit(e){
         firstnameinput.value='';
         lastnameinput.value='';
         }
+}
+
+}
+{
+//for login html & indexhtml
+
+const myform=document.querySelector('#my-form');
+const IDnameinput=document.querySelector('#IDname');
+const emailinput=document.querySelector('#email');
+const msg=document.querySelector('.msg');
+
+myform.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+
+    //form validation
+    if( IDnameinput.value === '' || emailinput.value ===''){
+        msg.classList.add('error');
+        msg.innerHTML='* please enter all field';
+        setTimeout(()=>msg.remove(),3000);
+    }else{
+        console.log("success");
+        window.location="todo.html";
+        //clear fields
+        IDnameinput.value='';
+        emailinput.value='';
+
+        }
+}
 }
 
